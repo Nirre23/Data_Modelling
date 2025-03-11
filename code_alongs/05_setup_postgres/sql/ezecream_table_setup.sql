a7f8c18897193a6ebdf2043e6ed94a8cc5553a41
Nirre23 <niclas.karlman@hotmail.com>
2025-03-11 15:32:02 +0100

    refactor SQL scripts for ezecream setup: enhance table structures and remove obsolete insert scripts

--- code_alongs/05_setup_postgres/sql/ezecream_table_setup.sql
+++ code_alongs/05_setup_postgres/sql/ezecream_table_setup.sql
@@ -1,38 +1,38 @@
 CREATE SCHEMA IF NOT EXISTS ezecream_05;
 
-SET
-    search_path TO ezecream_05;
+SET search_path TO ezecream_05;
 
-CREATE TABLE IF NOT EXISTS
-    Customer (
-        customer_id SERIAL PRIMARY KEY,
-        first_name VARCHAR(50),
-        last_name VARCHAR(50),
-        address VARCHAR(100),
-        email VARCHAR(255) UNIQUE
-    );
+-- Customer Table
+CREATE TABLE IF NOT EXISTS Customer (
+    customer_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    address VARCHAR(100) NOT NULL
+);
 
-CREATE TABLE IF NOT EXISTS
-    Orders (
-        order_id SERIAL PRIMARY KEY,
-        customer_id INTEGER,
-        FOREIGN KEY (customer_id) REFERENCES Customer (customer_id),
-        order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    );
+-- Orders Table
+CREATE TABLE IF NOT EXISTS Orders (
+    order_id SERIAL PRIMARY KEY,
+    customer_id INTEGER NOT NULL,
+    order_date DATE DEFAULT CURRENT_DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customer (customer_id)
+);
 
-CREATE TABLE IF NOT EXISTS
-    Product (
-        product_id SERIAL PRIMARY KEY,
-        name VARCHAR(50),
-        price INTEGER
-    );
+-- OrderLine Table
+CREATE TABLE IF NOT EXISTS OrderLine (
+    orderline_id SERIAL PRIMARY KEY,
+    order_id INTEGER NOT NULL,
+    product_id INTEGER NOT NULL,
+    quantity INTEGER,
+    FOREIGN KEY (order_id) REFERENCES Orders (order_id),
+    FOREIGN KEY (product_id) REFERENCES Product (product_id)
+);
 
-CREATE TABLE IF NOT EXISTS
-    OrderLine (
-        orderline_id SERIAL PRIMARY KEY,
-        order_id INTEGER,
-        product_id INTEGER,
-        quantity INTEGER,
-        FOREIGN KEY (order_id) REFERENCES Orders (order_id),
-        FOREIGN KEY (product_id) REFERENCES Product (product_id)
-    );
+-- Product Table
+CREATE TABLE IF NOT EXISTS Product (
+    product_id SERIAL PRIMARY KEY,
+    name VARCHAR(30) NOT NULL,
+    price DOUBLE NOT NULL
+);
+-- TODO for reader: implement the rest of the physical logical diagram
